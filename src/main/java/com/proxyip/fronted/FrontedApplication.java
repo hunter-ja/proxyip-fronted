435014183c7cc8116c45c7abb93debff67a1b610
Hunter <Hunter@QQC88.net>
2019-12-10 14:42:05 +0800

    init

---
+++ src/main/java/com/proxyip/fronted/FrontedApplication.java
@@ -0,0 +1,13 @@
+package com.proxyip.fronted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proxyip/fronted/FrontedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proxyip.fronted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
